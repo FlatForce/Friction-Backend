6501069c5a94422e6cf9a77db20729098d37cfb8
Brendan Nason <flatrotech@gmail.com>
2022-07-23 21:28:45 -0500

    try to create lambda

--- functions/lambda-template/arch/main.tf
+++ functions/lambda-template/arch/main.tf
@@ -14,28 +14,27 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
-resource "aws_lambda_function" "test_lambda" {
-  depends_on = [data.archive_file.python_lambda_package]
-  # If the file is not in the current working directory you will need to include a 
-  # path.module in the filename.
-  filename      = "lambda.zip"
-  function_name = "Friction_Test"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "main.lambda_handler"
+# resource "aws_lambda_function" "test_lambda" {
+#   # If the file is not in the current working directory you will need to include a 
+#   # path.module in the filename.
+#   filename      = "lambda.zip"
+#   function_name = "Friction_Test"
+#   role          = aws_iam_role.lambda_role.arn
+#   handler       = "main.lambda_handler"
 
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = filebase64sha256("lambda.zip")
+#   # The filebase64sha256() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+#   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+#   source_code_hash = filebase64sha256(data.archive_file.python_lambda_package.out)
 
-  runtime = "python3.6"
+#   runtime = "python3.6"
 
-  environment {
-    variables = {
-      foo = "bar"
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       foo = "bar"
+#     }
+#   }
+# }
 
 data "archive_file" "python_lambda_package" {  
   type = "zip"  
