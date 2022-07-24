4e267b98f1b2288275bb919b2873b8bf9cb74618
Brendan Nason <flatrotech@gmail.com>
2022-07-23 21:32:45 -0500

    try to create lambda

--- functions/lambda-template/arch/main.tf
+++ functions/lambda-template/arch/main.tf
@@ -14,30 +14,30 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
-# resource "aws_lambda_function" "test_lambda" {
-#   # If the file is not in the current working directory you will need to include a 
-#   # path.module in the filename.
-#   filename      = "lambda.zip"
-#   function_name = "Friction_Test"
-#   role          = aws_iam_role.lambda_role.arn
-#   handler       = "main.lambda_handler"
-
-#   # The filebase64sha256() function is available in Terraform 0.11.12 and later
-#   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-#   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-#   source_code_hash = filebase64sha256(data.archive_file.python_lambda_package.out)
-
-#   runtime = "python3.6"
-
-#   environment {
-#     variables = {
-#       foo = "bar"
-#     }
-#   }
-# }
-
 data "archive_file" "python_lambda_package" {  
   type = "zip"  
   source_file = "${path.module}/../src/main.py" 
   output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a 
+  # path.module in the filename.
+  filename      = "lambda.zip"
+  function_name = "Friction_Test"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "main.lambda_handler"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+
+  runtime = "python3.6"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
 }
